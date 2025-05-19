a804c534c58216889412357b387f87bb62646013
smoriginals <smoriginals@users.noreply.github.com>
2025-05-19 23:56:38 +0530

    update
    frontend in progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Card from './Components/Card';
 import './App.css';
+import Menu from './Components/Menu';
 
 function App() {
   return (
       <>
-        <Card/>
+        <Menu/>
+          
       </>
   );
 }
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,9 +1,31 @@
 import React from 'react'
-
+import '../App.css';
 export default function Menu() {
     return (
         <>
-
+            <div className='Main-Menu'>
+                <div className='Menu-Layout'>
+                    <div className='Menu-Buttons'>
+                        <div className='Left-one'></div>
+                        <div className='Right-one'></div>
+                    </div>
+                    <div className='Links'>
+                        <a href='/'><h3>Onedgfdgfdgdd</h3></a>
+                        <a href='/'><h3>Twodgfdgfdgdd</h3></a>
+                        <a href='/'><h3>Threedgfdgfdgdd</h3></a>
+                        <a href='/'><h3>Fourdgfdgfdgdd</h3></a>
+                        <a href='/'><h3>Fivedgfdgfdgdd</h3></a>
+                        <a href='/'><h3>Sixdgfdgfdgdd</h3></a>
+                        <a href='/'><h3>Sevendgfdgfdgdd</h3></a>
+                        <a href='/'><h3>Eightdgfdgfdgdd</h3></a>
+                        <a href='/'><h3>Onedgfdgfdgdd</h3></a>
+                        <a href='/'><h3>Twodgfdgfdgdd</h3></a>
+                        <a href='/'><h3>Threedgfdgfdgdd</h3></a>
+                        <a href='/'><h3>Fourdgfdgfdgdd</h3></a>
+                        <a href='/'><h3>Fivedgfdgfdgdd</h3></a>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
